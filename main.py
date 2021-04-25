9b6123b5918ac6adb12f8f9ed99d3eac0d1ef2aa
Oleg Modenov <olegmodenov@gmail.com>
2021-04-25 20:40:47 +0500

    Нужно переписать класс Ship, так как сейчас неудобно создавать объекты этого класса

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random
+from random import randint
 
 
 # class BoardOutException(Exception):
@@ -10,6 +10,8 @@ import random
 
 
 class Dot:
+    near_dots = []
+
     def __init__(self, x, y, state=' '):
         self.x = x
         self.y = y
@@ -24,11 +26,34 @@ class Dot:
     def __str__(self):
         return f"{self.x}:{self.y}"
 
+    def get_near_dots(self):
+        self.near_dots = [
+            Dot(self.x + 1, self.y),
+            Dot(self.x - 1, self.y),
+            Dot(self.x + 1, self.y - 1),
+            Dot(self.x - 1, self.y - 1),
+            Dot(self.x + 1, self.y + 1),
+            Dot(self.x - 1, self.y + 1),
+            Dot(self.x, self.y + 1),
+            Dot(self.x, self.y - 1)
+        ]
+
 
 class Ship:
+    near_dots = []
+
     def __init__(self, dots, hp):
         self.dots = dots
         self.hp = hp
+        self.direction = randint(1, 2)
+
+    def get_near_dots(self):
+        """ Возвращает список точек вокруг корабля """
+        for ship_dot in self.dots:
+            ship_dot.get_near_dots()
+            for near_dot in ship_dot.near_dots:
+                if near_dot not in self.near_dots:
+                    self.near_dots.append(near_dot)
 
 
 class Board:
@@ -45,17 +70,14 @@ class Board:
         """
         self.ships.append(ship)
         # Сравниваются список точек корабля и список списков точек доски. Возможно, можно оптимизировать
-        for i in range(len(ship.dots)):
+        for ship_dot in ship.dots:
             for line in self.dots:
                 for dot in line:
-                    if dot == ship.dots[i]:
+                    if dot == ship_dot:
                         dot.state = '\N{Black Circle}'
 
-    def contour(self):
-        """ Возвращает список точек вокруг корабля (на этих точках не может располагаться другой корабль) """
-        pass
-
     def shot(self, dot):
+        """ Меняет состояние точки в случае попадания """
         message = ''
 
         if dot.state == 'П':
@@ -96,35 +118,57 @@ class Board:
 
 
 class Game:
-    def __init__(self, board):
-        self.board = board
+    # def __init__(self, board):
+    #     self.board = board
 
-    def random_board(self):
-        pass
+    def generate_random_board(self):
+        random_board = Board([[Dot(x + 1, y + 1) for x in range(6)] for y in range(6)])
+
+        for i in range(4):
+            ship1 = Ship([Dot(randint(1, 6), randint(1, 6))], 1)
+            random_board.add_ship(ship1)
+
+        ship2_1 = Ship([Dot(x + 1, 1) for x in range(2)], 2)
+        ship2_2 = Ship([Dot(x + 1, 1) for x in range(2)], 2)
+        #
+        # ship3 = Ship([Dot(5, y + 2) for y in range(3)], 3)
+        random_board.print_board()
+        #
+        random_board.add_ship(ship2_1)
+        random_board.add_ship(ship2_2)
+        # random_board.add_ship(ship3)
+        # random_board.print_board()
 
 
-# print('\N{Black Circle}') # корабль
-# print('\N{Circle with Horizontal Bar}') # подбитый корабль
+game = Game()
+game.generate_random_board()
 
-# Тест - создание доски, 3 разных кораблей и вывод
-board1 = Board([[Dot(x + 1, y + 1) for x in range(6)] for y in range(6)])
-ship1 = Ship([Dot(3, 3)], 1)
-ship2 = Ship([Dot(x + 1, 1) for x in range(2)], 2)
-ship3 = Ship([Dot(5, y + 2) for y in range(3)], 3)
-board1.print_board()
+# # Тест - создание списка ближайших точек для точки
+# dot1 = Dot(0, 0)
+# dot1.get_near_dots()
+# print(dot1.near_dots)
 
-# Тест - добавление кораблей на доску
-board1.add_ship(ship1)
-board1.add_ship(ship2)
-board1.add_ship(ship3)
-board1.print_board()
+# # Тест - создание доски, 3 разных кораблей и вывод пустой доски
+# board1 = Board([[Dot(x + 1, y + 1) for x in range(6)] for y in range(6)])
+# ship1 = Ship([Dot(3, 3)], 1)
+# ship2 = Ship([Dot(x + 1, 1) for x in range(2)], 2)
+# ship3 = Ship([Dot(5, y + 2) for y in range(3)], 3)
+# board1.print_board()
 
-# Тест - метод shot
-board1.shot(board1.dots[0][0])
-board1.print_board()
-board1.shot(board1.dots[4][4])
-board1.print_board()
-board1.shot(board1.dots[4][4])
-board1.print_board()
+# # Тест - добавление кораблей на доску и вывод
+# board1.add_ship(ship1)
+# board1.add_ship(ship2)
+# board1.add_ship(ship3)
+# board1.print_board()
+#
+# # Тест - метод shot
+# board1.shot(board1.dots[0][0])
+# board1.print_board()
+# board1.shot(board1.dots[4][4])
+# board1.print_board()
+# board1.shot(board1.dots[4][4])
+# board1.print_board()
 
-# print(board1.__dict__)
+# # Тест - создание списка ближайших точек для корабля
+# ship3.get_near_dots()
+# print(len(ship3.near_dots))
